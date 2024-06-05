416e442f67799d4161a0d7b4392cdaec4475dff4
Paulo Dantas <paulodantasbjr@gmail.com>
2024-06-05 13:02:01 +0100

    add update at

--- src/components/task-item.tsx
+++ src/components/task-item.tsx
@@ -1,6 +1,7 @@
 import { Checkbox, IconButton, Paper } from "@mui/material";
 import { Delete as DeleteIcon, Edit as EditIcon } from "@mui/icons-material/";
 import { cn } from "@/utils";
+import { format } from "date-fns";
 
 interface TaskItemProps {
   task: {
@@ -8,6 +9,7 @@ interface TaskItemProps {
     name: string;
     description?: string | undefined;
     active?: boolean | undefined;
+    updatedAt?: Date | undefined;
   };
   onDeleteTask: (id: string) => void;
   onEditTask: (id: string) => void;
@@ -38,14 +40,20 @@ export function TaskItem({
         />
       </div>
       <div className="flex grow flex-col gap-2">
-        <h2
-          className={cn(
-            "text-lg font-semibold text-purple-500",
-            task.active && "line-through"
-          )}
-        >
-          {task.name}
-        </h2>
+        <div className="flex items-center justify-between">
+          <h2
+            className={cn(
+              "text-lg font-semibold text-purple-500",
+              task.active && "line-through"
+            )}
+          >
+            {task.name}
+          </h2>
+
+          <span className="text-xs text-slate-400">
+            {format(task.updatedAt!, "MM/dd/yyyy")}
+          </span>
+        </div>
         <p className="text-xs text-slate-400">{task.description}</p>
       </div>
       <div className="flex items-center gap-2">
